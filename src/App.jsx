a5972c76b7dfb506d0c2eea1067fb256424a8c88
unknown <7197888@gmail.com>
2024-06-28 02:36:48 -0400

    06282024

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import ItemList from './components/ItemList'
-import ItemApp from './components/ItemApp'
+import ItemListFunc from './components/increaseQuantity'
 
-function App() {
+
+export default function ItemsName() {
+  const ItemsArr = [
+      { name: 'Apples', Quantity: 0, Price: 1.5 },
+      { name: 'Bananas', Quantity: 0, Price: 0.8 },
+      { name: 'Oranges', Quantity: 0, Price: 1.2 },
+      { name: 'Strawberries', Quantity: 0, Price: 3.0 },
+      { name: 'Grapes', Quantity: 0, Price: 2.5 },
+      { name: 'Carrots', Quantity: 0, Price: 0.7 },
+      { name: 'Broccoli', Quantity: 0, Price: 1.8 },
+      { name: 'Spinach', Quantity: 0, Price: 2.0 },
+      { name: 'Potatoes', Quantity: 0, Price: 0.6 },
+      { name: 'Tomatoes', Quantity: 0, Price: 1.1 },
+      { name: 'Milk', Quantity: 0, Price: 1.3 },
+      { name: 'Cheese', Quantity: 0, Price: 4.5 },
+      { name: 'Yogurt', Quantity: 0, Price: 0.9 },
+      { name: 'Butter', Quantity: 0, Price: 2.2 },
+      { name: 'Ice cream', Quantity: 0, Price: 3.5 }
+
+  ]
+
+
   return (
-    <>
-      <ItemApp />
-    </>
-  )
-}
+      <>
+         
+          <ItemListFunc ItemsArr={ItemsArr} Quantity={ItemsArr.Quantity} />
+      </>
+  );
 
-export default App
+}
