5925d8a4cd97251cfa552852805c11614ded8b27
unknown <7197888@gmail.com>
2024-07-01 00:43:05 -0400

    07012024

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,12 @@ export default function App() {
 
   return (
     <>
+      <p>Click on SHOW to see the department</p>
+      <p>To start adding a product, click on the box "Click here to add the Item"
+        <br />
+        After that add more click on the arrow to remove the product completely click on Remove
+
+      </p>
       <Fruit />
       <Frozen />
       <Hygiene />
